07b153b3b353363c32a908645c237e019add5dd5
Terence Truong <terence.v.truong@gmail.com>
2023-10-06 12:51:25 +0700

    edit scripts

--- iron-jammy/scripts/install-ros2.sh
+++ iron-jammy/scripts/install-ros2.sh
@@ -5,3 +5,6 @@ sudo NEEDRESTART_MODE=a apt-get install ros-dev-tools -y
 sudo NEEDRESTART_MODE=a apt-get install ros-iron-desktop -y
 ##sudo apt install ros-iron-ros-base
 sudo NEEDRESTART_MODE=a apt-get install ~nros-iron-rqt* -y
+
+echo "source /opt/ros/iron/setup.bash" >> /home/vagrant/.profile
+touch /home/vagrant/.Xauthority
--- iron-jammy/scripts/prep-system.sh
+++ iron-jammy/scripts/prep-system.sh
@@ -5,16 +5,9 @@ sudo locale-gen en_US en_US.UTF-8
 sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
 
-sudo apt-get install software-properties-common -y
 sudo add-apt-repository universe -y
 
 sudo apt-get update && sudo apt-get install curl -y
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
-
-sudo apt-get update
-sudo NEEDRESTART_MODE=a apt-get upgrade -y
-
-echo "source /opt/ros/iron/setup.bash" >> /home/vagrant/.bashrc
-touch /home/vagrant/.Xauthority
